1993433f0dd183c8d2582b64ba7f40bab0efffe6
Sodiqjon1998 <sirojiddinovsodiqjon1998@gmail.com>
2024-05-19 17:27:23 +0500

    available

--- resources/views/student/exam/show.blade.php
+++ resources/views/student/exam/show.blade.php
@@ -4,46 +4,46 @@
 
     <div class="container">
 
-        Xush kelibsiz! {{Auth::user()->name}} <img src="{{asset('images/staticImages/welStudent.png')}}" width="35px"
-                                                   alt="">
-        <h1 class="display-6 text-center">{{$exam[0]['name']}} <img
-                src="{{asset('images/staticImages/book.png')}}" alt="" width="45px"></h1>
+        Xush kelibsiz! {{ Auth::user()->name }} <img src="{{ asset('images/staticImages/welStudent.png') }}" width="35px"
+            alt="">
+        <h1 class="display-6 text-center">{{ $exam[0]['name'] }} <img src="{{ asset('images/staticImages/book.png') }}"
+                alt="" width="45px"></h1>
         @php
             $time = explode(':', $exam[0]['time']);
             $date = explode('-', $exam[0]['date']);
-
         @endphp
 
         @php $qcount = 1; @endphp
-        @if($success == true)
-            @if(count($qnAnswers) > 0)
+        @if ($success == true)
+            @if (count($qnAnswers) > 0)
                 <h6 class="text-right">
-                    <img src="{{asset('images/staticImages/time.png')}}" width="25px" alt="">
+                    <img src="{{ asset('images/staticImages/time.png') }}" width="25px" alt="">
                     <span class="time">
-                        {{$exam[0]['time']}}
+                        {{ $exam[0]['time'] }}
                     </span>
                 </h6>
-                <form action="{{route('submitExam')}}" method="POST" id="exam_form" class="mb-5">
+                <form action="{{ route('submitExam') }}" method="POST" id="exam_form" class="mb-5">
                     @csrf
-                    <input type="hidden" name="exam_id" id="" value="{{$exam[0]['id']}}">
-                    @foreach($qnAnswers as $key => $question)
+                    <input type="hidden" name="exam_id" id="" value="{{ $exam[0]['id'] }}">
+                    @foreach ($qnAnswers as $key => $question)
                         <div class="card">
                             <h5 class="card-header">
-                                Q.{{$qcount++}} {{$question['questionItems'][0]['question']}}
-                                <img src="{{asset('images/staticImages/shot.png')}}" width="30px" class="float-end"
-                                     alt="">
+                                Q.{{ $qcount++ }} {{ $question['questionItems'][0]['question'] }}
+                                <img src="{{ asset('images/staticImages/shot.png') }}" width="30px" class="float-end"
+                                    alt="">
                             </h5>
-                            <input type="hidden" name="q[]" id="" value="{{$question['questionItems'][0]['id']}}">
-                            <input type="hidden" name="ans_{{$qcount-1}}" id="ans_{{$qcount-1}}">
+                            <input type="hidden" name="q[]" id=""
+                                value="{{ $question['questionItems'][0]['id'] }}">
+                            <input type="hidden" name="ans_{{ $qcount - 1 }}" id="ans_{{ $qcount - 1 }}">
                             @php $acount = 1; @endphp
                             <div class="card-body">
                                 <ol type="A">
-                                    @foreach($question['questionItems'][0]['answers'] as $k => $answer)
+                                    @foreach ($question['questionItems'][0]['answers'] as $k => $answer)
                                         <li style="font-weight: lighter;">
-                                            <input type="radio" name="radio_{{$qcount-1}}" id=""
-                                                   class="select_ans"
-                                                   value="{{$answer->id}}" data-id="{{$qcount-1}}"> &nbsp;
-                                            {{$answer->answer}}
+                                            <input type="radio" name="radio_{{ $qcount - 1 }}" id=""
+                                                class="select_ans" value="{{ $answer->id }}"
+                                                data-id="{{ $qcount - 1 }}"> &nbsp;
+                                            {{ $answer->answer }}
                                         </li>
                                     @endforeach
                                 </ol>
@@ -52,7 +52,7 @@
                     @endforeach
                     <div class="text-center">
                         <input type="submit" name="" id="sendAnswer" class="btn btn-success btn-sm"
-                               value="Testni yakunlash">
+                            value="Testni yakunlash">
                     </div>
                 </form>
             @else
@@ -62,116 +62,149 @@
             @endif
         @else
             <h6 class="text-danger display-6 text-center">
-                {{$msg}}
+                {{ $msg }}
             </h6>
         @endif
     </div>
 
     <script>
+        $(document).ready(function() {
 
-        $(document).ready(function () {
-
-            $(".select_ans").click(function () {
+            $(".select_ans").click(function() {
                 let no = $(this).attr('data-id');
                 let val = $("#ans_" + no).val($(this).val());
             });
 
-            let date = @json($exam[0]['date']);
+            /**
+             * Javascripdata xizirgi vaqtni olish kodi
+             * */
+            let examId = {{ $exam[0]['id'] }}
+            let examDate = @json($date)
 
-            let bugun = new Date();
 
-            let month = bugun.getMonth() + 1;
-            let day = bugun.getDate();
+            let today = new Date();
+            let month = today.getMonth() + 1;
+            let day = today.getDate();
+            let date = today.getFullYear() + '-' + month.toString().padStart(2, '0') + '-' + day.toString()
+                .padStart(2, '0');
 
-            let sana = bugun.getFullYear() + '-' + month.toString().padStart(2, '0') + '-' + day.toString().padStart(2, '0');
+            let localTime = localStorage.getItem(examId)
 
-            let getData = localStorage.getItem({{$exam[0]['id']}});
-            let parseData = JSON.parse(getData);
-            // console.log(as[{{$exam[0]['id']}}].second)
+            localTime = JSON.parse(localTime);
 
-            if (sana == date) {
+
+
+            if (localTime != null ) {
                 let time = @json($time);
 
-
-                let storedHours = localStorage.getItem("hours"); //parseData[{{$exam[0]['id']}}].hours
-                let storedMinutes = localStorage.getItem("minutes"); //parseData[{{$exam[0]['id']}}].minutes
-                let storedSecond = localStorage.getItem("second"); //parseData[{{$exam[0]['id']}}].second
-
-                let hours = storedHours ? parseInt(storedHours) : parseInt(time[0]);
-                let minutes = storedMinutes ? parseInt(storedMinutes) : parseInt(time[1]);
-                let second = storedSecond ? parseInt(storedSecond) : 59;
+                let second = localTime[examId].second ? localTime[examId].second : 59;
+                let minutes = localTime[examId].minutes ? localTime[examId].minutes : parseInt(time[1]);
+                let hours = localTime[examId].hours ? localTime[examId].hours : parseInt(time[0]);
 
                 $('.time').text(hours + ':' + minutes + ':' + second);
 
                 function startTimer() {
-                    let timer = setInterval(function () {
-
+                    let timer = setInterval(() => {
                         let data = {};
-                        data[{{ $exam[0]['id'] }}] = {
-                            'second': second.toString(),
-                            'minutes': minutes.toString(),
-                            'hours': hours.toString(),
+                        data[examId] = {
+                            'id': examId,
+                            'second': second,
+                            'minutes': minutes,
+                            'hours': hours
                         };
 
-                        localStorage.setItem('question', JSON.stringify(data));
-
-                        // localStorage.setItem("second", second.toString());
-                        // localStorage.setItem("minutes", minutes.toString());
-                        // localStorage.setItem("hours", hours.toString());
+                        localStorage.setItem(examId, JSON.stringify(data));
 
                         if (hours == 0 && minutes == 0 && second == 0) {
                             clearInterval(timer);
-                            localStorage.removeItem({{$exam[0]['id']}});
+                            localStorage.remove(examId)
                             $("#exam_form").submit();
                         }
 
-                        if (second <= 0) {
-                            minutes--;
-                            second = 59;
-                        }
-
-                        if (minutes <= 0 && hours != 0) {
+                        if (minutes == 0 && second == 0) {
                             hours--;
                             minutes = 59;
                             second = 59;
                         }
 
-                        let tempHours = hours.toString().padStart(2, '0');
-                        let tempMinutes = minutes.toString().padStart(2, '0');
-                        let tempSecond = second.toString().padStart(2, '0');
-
-                        $('.time').text(tempHours + ':' + tempMinutes + ':' + tempSecond);
-
-
+                        if (second == 0) {
+                            minutes--;
+                            second = 59;
+                        }
+                        // console.log(second + "--" + minutes + "--" + hours);
+                        $('.time').text(hours.toString().padStart(2, '0') + ':' + minutes.toString()
+                            .padStart(2, '0') + ':' + second.toString().padStart(2, '0'));
                         second--;
                     }, 1000);
-
-                    $("#sendAnswer").click(function (e) {
+                    $("#sendAnswer").click(function(e) {
                         e.preventDefault();
                         let check = confirm("Testni yakunlamoqchimisiz?");
 
                         if (check) {
                             clearInterval(timer); // timer ni avtomatik to'zalash
-                            localStorage.removeItem({{$exam[0]['id']}});
+                            localStorage.removeItem({{ $exam[0]['id'] }});
                             $("#exam_form").submit();
                         }
                     });
                 }
+            } else {
+                let time = @json($time);
 
+                let second = 59;
+                let minutes = parseInt(time[1]);
+                let hours = parseInt(time[0]);
 
-                startTimer();
+                $('.time').text(hours + ':' + minutes + ':' + second);
+
+                function startTimer() {
+                    let timer = setInterval(() => {
+
+                        let data = {};
+
+                        data[examId] = {
+                            'id': examId,
+                            'second': second,
+                            'minutes': minutes,
+                            'hours': hours
+                        };
+
+                        localStorage.setItem(examId, JSON.stringify(data));
+
+                        if (hours == 0 && minutes == 0 && second == 0) {
+                            clearInterval(timer);
+                            localStorage.remove(examId)
+                            $(".time").text("Tugadi")
+                        }
+
+                        if (minutes == 0 && second == 0) {
+                            hours--;
+                            minutes = 59;
+                            second = 59;
+                        }
+
+                        if (second == 0) {
+                            minutes--;
+                            second = 59;
+                        }
+                        // console.log(second + "--" + minutes + "--" + hours);
+                        $('.time').text(hours + ':' + minutes + ':' + second);
+                        second--;
+                    }, 1000);
+                }
             }
+            startTimer();
 
         });
 
         function isValid() {
             let result = true;
-            let qLength = parseInt("{{$qcount}}") - 1;
+            let qLength = parseInt("{{ $qcount }}") - 1;
 
             for (let i = 1; i <= qLength; i++) {
                 if ($('#ans_' + i).val() == "") {
                     result = false;
-                    $("#ans_" + i).parent().append("<span class='btn btn-warning btn-sm mb-3 ml-3 error_msg'>Javob tanlang!</span>");
+                    $("#ans_" + i).parent().append(
+                        "<span class='btn btn-warning btn-sm mb-3 ml-3 error_msg'>Javob tanlang!</span>");
                     setTimeout(() => {
                         $(".error_msg").remove();
                     }, 5000);
@@ -180,7 +213,6 @@
 
             return result;
         }
-
     </script>
 
 @endsection
